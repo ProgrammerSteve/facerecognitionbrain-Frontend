e481232d41e97276ccb9d2a7a9c0a52dc19572b7
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-08-15 06:10:54 -0500

    switching backend to heroku instead of AWS

--- src/url-file.js
+++ src/url-file.js
@@ -5,4 +5,7 @@ const localURL='http://localhost:3001';
 //AWS
 const AWSURL='http://ec2-34-203-234-84.compute-1.amazonaws.com:3001';
 
-export const backendURL=AWSURL;
+//Heroku
+const Heroku="https://smartbrain-app-backend.herokuapp.com"
+
+export const backendURL=Heroku;
