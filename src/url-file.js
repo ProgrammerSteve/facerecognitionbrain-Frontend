c5e99b4365466eab20d3c14a654cb78c0707cef4
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-08-26 11:43:13 -0500

    fixing small typo in url

--- src/url-file.js
+++ src/url-file.js
@@ -5,10 +5,10 @@ const localURL='http://localhost:3001';
 //AWS
 const AWSURL='http://ec2-34-203-234-84.compute-1.amazonaws.com:3001';
 
-//Heroku
+//Heroku (not free anymore so I'll stop using it)
 const Heroku="https://smartbrain-app-backend.herokuapp.com"
 
 //Render.com
-const RenderURL="https://smartbrain-backend.onrender.com/"
+const RenderURL="https://smartbrain-backend.onrender.com"
 
 export const backendURL=RenderURL;
